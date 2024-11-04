cda7b4146c9e926c46bb5ad0c83d1091923c96fe
mmishkina <mmishkina@ozon.ru>
2024-11-05 00:41:43 +0300

    added 6 lesson task

---
+++ selenium_practice/lesson_6.py
@@ -0,0 +1,26 @@
+from attr import attributes
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(options=options)
+wait = WebDriverWait(driver, timeout=25, poll_frequency=0)
+
+driver.get('https://omayo.blogspot.com/')
+
+# LOCATORS
+disappear_text_after_25_seconds = ('xpath', '//div[@id = "deletesuccess"]')
+displayed_text_after_10_seconds = ('xpath', '//div[@id = "delayedText"]')
+enabled_button_after_5_seconds = ('xpath', '//input[@id = "timerButton"]')
+try_it_button = ('xpath', '//button[contains(@onclick, "setTimeout")][text() = "Try it"]')
+disabled_button = ('xpath', '//button[@id = "myBtn"]')
+
+wait.until(EC.invisibility_of_element_located(disappear_text_after_25_seconds), 'Элемент найден на странице')
+
+wait.until(EC.visibility_of_element_located(displayed_text_after_10_seconds), 'Элемент не найден на странице')
+
+wait.until(EC.visibility_of_element_located(enabled_button_after_5_seconds), 'Элемент не найден на странице').is_enabled()
+
+wait.until(EC.visibility_of_element_located(try_it_button)).click()
+wait.until(EC.element_attribute_to_include(disabled_button, "disabled"), 'Кнопка в состоянии enabled')
